fadf9e708ebc0f3f5538e0a59873bbfae4a9e690
VaIeroK <kondratenko449@gmail.com>
2022-07-29 19:19:13 +0300

    Добавил меньшее ограничение полигонов для сплита, выбивало на некоторых моделях

--- Source/Editors/XrECore/Editor/ExportSkeleton.h
+++ Source/Editors/XrECore/Editor/ExportSkeleton.h
@@ -173,7 +173,7 @@ protected:
             }
         }
 
-        bool GetVertexBound         () { return m_Verts.size() < u16(-1); }
+        bool GetVertexBound         () { return m_Verts.size() < 60000; }
     };
 	DEFINE_VECTOR		(SSplit,SplitVec,SplitIt);
 	SplitVec			m_Splits;
