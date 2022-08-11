a250eacba6173f206b9ae2d562e09281e669169b
VaIeroK <kondratenko449@gmail.com>
2022-08-11 11:23:31 +0300

    Фикс

--- Source/Editors/XrECore/Editor/EditObjectIO.cpp
+++ Source/Editors/XrECore/Editor/EditObjectIO.cpp
@@ -242,7 +242,6 @@ void CEditableObject::Save(IWriter& F)
 bool CEditableObject::Load(IReader& F)
 {
     m_objectFlags.set(eoSoCRefs, FALSE);
-    m_objectFlags.set(eoDynamic, FALSE);
 	bool bRes = true;
 	do{
 		u32 version = 0;
@@ -258,6 +257,7 @@ bool CEditableObject::Load(IReader& F)
 		}
 
 		R_ASSERT(F.r_chunk(EOBJ_CHUNK_FLAGS, &m_objectFlagsForSave.flags));
+        m_objectFlags.set(eoDynamic, m_objectFlagsForSave.is(eoDynamic));
 
 		if (F.find_chunk	(EOBJ_CHUNK_CLASSSCRIPT))
 			F.r_stringZ		(m_ClassScript);
@@ -364,7 +364,6 @@ bool CEditableObject::Load(IReader& F)
             IReader* B_CHUNK = F.open_chunk(EOBJ_CHUNK_BONES2);
             if (B_CHUNK)
 			{
-                m_objectFlags.set(eoDynamic, TRUE);
                 int chunk = 0;
                 IReader* O;
                 while (0!=(O=B_CHUNK->open_chunk(chunk++)))
@@ -376,7 +375,6 @@ bool CEditableObject::Load(IReader& F)
                 B_CHUNK->close();
                 PrepareBones();
             }else if (F.find_chunk(EOBJ_CHUNK_BONES)){
-                m_objectFlags.set(eoDynamic, TRUE);
                 m_Bones.resize(F.r_u32());
                 for (BoneIt b_it=m_Bones.begin(); b_it!=m_Bones.end(); b_it++){
                     *b_it = xr_new<CBone>();
