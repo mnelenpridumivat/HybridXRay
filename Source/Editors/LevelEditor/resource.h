fe2b2fe35a6eb7d4a1a32de8023ba93f6ad32ab1
Pavel Spitsin <pavel.spitsin@yandex.ru>
2023-01-22 12:37:34 +0300

    [ActorEditor|LevelEditor|ParticleEditor|ShaderEditor] Исправлены иконки.

--- Source/Editors/LevelEditor/resource.h
+++ Source/Editors/LevelEditor/resource.h
@@ -1,6 +1,6 @@
 //{{NO_DEPENDENCIES}}
-// включаемый файл, созданный в Microsoft Visual C++.
-// используется LevelEditor.rc
+// Microsoft Visual C++ generated include file.
+// Used by LevelEditor.rc
 //
 #define IDI_ICON1                       106
 
@@ -8,7 +8,7 @@
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        107
+#define _APS_NEXT_RESOURCE_VALUE        108
 #define _APS_NEXT_COMMAND_VALUE         40001
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
