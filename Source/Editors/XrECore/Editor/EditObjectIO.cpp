88bdbf9c428ea708254ea1ffda887ec1d52d5504
Romann <kriworuck.production@yandex.ru>
2023-05-29 05:15:40 +0300

    * [VaIeroK] Фикс.
    ** Автор: VaIeroK <kondratenko449@gmail.com>

--- Source/Editors/XrECore/Editor/EditObjectIO.cpp
+++ Source/Editors/XrECore/Editor/EditObjectIO.cpp
@@ -227,7 +227,6 @@ void CEditableObject::Save(IWriter& F)
 bool CEditableObject::Load(IReader& F)
 {
     m_objectFlags.set(eoSoCRefs, FALSE);
-    m_objectFlags.set(eoDynamic, FALSE);
     bool bRes = true;
     do
     {
@@ -243,6 +242,7 @@ bool CEditableObject::Load(IReader& F)
         }
 
         R_ASSERT(F.r_chunk(EOBJ_CHUNK_FLAGS, &m_objectFlags.flags));
+        m_objectFlags.set(eoDynamic, m_objectFlags.is(eoDynamic));
 
         if (F.find_chunk(EOBJ_CHUNK_CLASSSCRIPT))
             F.r_stringZ(m_ClassScript);
@@ -367,7 +367,6 @@ bool CEditableObject::Load(IReader& F)
             IReader* B_CHUNK = F.open_chunk(EOBJ_CHUNK_BONES2);
             if (B_CHUNK)
             {
-                m_objectFlags.set(eoDynamic, TRUE);
                 int      chunk = 0;
                 IReader* O;
                 while (0 != (O = B_CHUNK->open_chunk(chunk++)))
@@ -381,7 +380,6 @@ bool CEditableObject::Load(IReader& F)
             }
             else if (F.find_chunk(EOBJ_CHUNK_BONES))
             {
-                m_objectFlags.set(eoDynamic, TRUE);
                 m_Bones.resize(F.r_u32());
                 for (BoneIt b_it = m_Bones.begin(); b_it != m_Bones.end(); b_it++)
                 {
