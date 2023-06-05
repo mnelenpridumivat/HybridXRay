952d8c12472c45476fe6c78645e0f8f48d41efa4
VaIeroK <kondratenko449@gmail.com>
2023-06-06 01:21:42 +0300

    Флаг автодетекта не нужен

--- Source/Editors/XrECore/Editor/EditObject.h
+++ Source/Editors/XrECore/Editor/EditObject.h
@@ -282,9 +282,8 @@ public:
         eoNormals       = (1 << 10),
         eoSoCSmooth     = (1 << 11),
         eoCoPSmooth     = (1 << 12),
-        eoAutoSmooth    = (1 << 13),
-        eoSoCInfluence  = (1 << 14),
-        eoSoCRefs       = (1 << 15),
+        eoSoCInfluence  = (1 << 13),
+        eoSoCRefs       = (1 << 14),
         eoFORCE32       = u32(-1)
     };
     IC BOOL IsDynamic()
