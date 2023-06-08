d8fb92e29517ef92e123b9616ef1a87d984f967d
Romann <kriworuck.production@yandex.ru>
2023-06-08 03:23:45 +0300

    * Исправление некоторых комментариев.

--- Source/XrGameSOC/ui/UIButton.h
+++ Source/XrGameSOC/ui/UIButton.h
@@ -15,7 +15,7 @@ public:
 	virtual bool	OnMouse					(float x, float y, EUIMessages mouse_action);
 	virtual void	OnClick					();
 
-	//���������� ����
+    // прорисовка окна
 	virtual void	DrawTexture				();
 	virtual void	DrawText				();
 	virtual void	DrawHighlightedText		();
@@ -31,31 +31,31 @@ public:
 				 DOWN_PRESS    //����� ��� �������
 			} E_PRESS_MODE;
 
-
-	//������ ����������� ���������
+    // заново подготовить состояние
     virtual void	Reset					();
-
 
 	//��������� �� ����� �� ������
 	// �������������� ���������
 	virtual void	HighlightItem			(bool bHighlight)			{m_bCursorOverWindow = bHighlight; }
 
-	//��������� � ������� ��������� ������
-	typedef enum{BUTTON_NORMAL, //������ ����� �� �������������
-		BUTTON_PUSHED, //� ������� ��������
-		BUTTON_UP      //��� ������������ ������ ���� 
+    // состояния в которых находится кнопка
+	typedef enum
+	{
+		BUTTON_NORMAL,   // кнопка никак не затрагивается
+		BUTTON_PUSHED,   // в нажатом сотоянии
+		BUTTON_UP        // при удерживаемой кнопки мыши
 	} E_BUTTON_STATE;
 
-	// ��������� ��������� ������: ��������, �� ��������
+    // Установка состояния кнопки: утоплена, не утоплена
 	void				SetButtonMode			(E_BUTTON_STATE eBtnState)	{ m_eButtonState = eBtnState; }
 	E_BUTTON_STATE		GetButtonsState			()							{ return m_eButtonState;}
 
-	// ��������� ������ ��� ������������� ����������� ���� ������ � ������ NORMAL_PRESS
+    // Поведение кнопки как переключателя реализовано пока только в режиме NORMAL_PRESS
 	void				SetButtonAsSwitch		(bool bAsSwitch)			{ m_bIsSwitch = bAsSwitch; }
 
-	// ������ � �������������
-	// ��� ������������ ������� �� ����� dinput.h, �� DirectX SDK.
-	// ��������: ������ A - ��� 0x1E(DIK_A)
+    // Работа с акселератором
+    // Код акселератора берется из файла dinput.h, из DirectX SDK.
+    // Например: кнопка A - код 0x1E(DIK_A)
 	void				SetAccelerator			(int iAccel, int idx)	{VERIFY(idx==0||idx==1); m_uAccelerator[idx] = iAccel; }
 	const int			GetAccelerator			(int idx) const			{VERIFY(idx==0||idx==1); return m_uAccelerator[idx]; }
 	IC bool				IsAccelerator			(int iAccel) const		{return (m_uAccelerator[0]==iAccel)||(m_uAccelerator[1]==iAccel) ;}
