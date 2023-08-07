07e9263235b80af81c48e2974e7a7274c48ff6c5
Romann <kriworuck.production@yandex.ru>
2023-08-07 11:50:04 +0300

    * [Actions]  Update.

--- utils/misc/xr_pack_build.cmd
+++ utils/misc/xr_pack_build.cmd
@@ -29,6 +29,8 @@ if %CONFIGURATION%==Release (
 7z a "HybridXRay.%EDITION_NAME%.7z" Bin\%PLATFORM%\%CONFIGURATION%\*.dll Bin\%PLATFORM%\%CONFIGURATION%\*.exe -i!utils\image\Icon_HybridXRay.png -i!utils\oalinst.exe -i!utils\tool_compile_xrAI.cmd -i!utils\tool_compile_xrAI_draft.cmd -i!utils\tool_compile_xrDO.cmd -i!utils\tool_compile_xrLC.cmd -i!utils\tool_create_spawn.cmd -i!utils\tool_verify_ai_map.cmd -i!_ActorEditor.cmd -i!_LevelEditor.cmd -i!_ParticleEditor.cmd -i!_ShaderEditor.cmd -i!Compiler_AI_map.cmd -i!Compiler_Batch.cmd -i!Compiler_Details.cmd -i!Compiler_Geometry.cmd -i!Compiler_Make_spawn.cmd -i!Compiler_Menu.cmd -i!Compiler_Readme.txt -i!fs.ltx -i!fs_cs.ltx -i!fs_soc.ltx -i!fsfactory.ltx -i!fsgame.ltx -i!fsgame_cs.ltx -i!fsgame_soc.ltx -i!License.txt -r0 "Bin\compilers_sky_x64\*" -i!gamedata -i!rawdata
 )
 if %CONFIGURATION%==Debug (
-7z a "HybridXRay.%EDITION_NAME%.7z" Bin\%PLATFORM%\%CONFIGURATION%\*.dll Bin\%PLATFORM%\%CONFIGURATION%\*.exe -i!Debug_ActorEditor.cmd -i!Debug_LevelEditor.cmd -i!Debug_ParticleEditor.cmd -i!Debug_ShaderEditor.cmd -i!License.txt -xr!.*
+7z a "HybridXRay.%EDITION_NAME%.7z" "Bin\%PLATFORM%\%CONFIGURATION%\*" -i!Debug_ActorEditor.cmd -i!Debug_LevelEditor.cmd -i!Debug_ParticleEditor.cmd -i!Debug_ShaderEditor.cmd -i!License.txt -xr!.*
 )
+if %CONFIGURATION%==Release (
 7z a "Symbols.%EDITION_NAME%.7z" Bin\%PLATFORM%\%CONFIGURATION%\*.pdb -i!License.txt -xr!.*
+)
