8cc5c18619a816355880e9d0d6d8147f2b3fd9df
Romann <kriworuck.production@yandex.ru>
2023-10-15 07:28:51 +0300

    * [LevelEditor]  Дописывать префикс уровня спаун-элементам под группой.
    - Но level_prefix из пысовских групп придётся ручками вырезать :-)
    -----------------
    Автор: SAAS

--- Source/Editors/LevelEditor/Editor/Entry/Group/GroupObject.cpp
+++ Source/Editors/LevelEditor/Editor/Entry/Group/GroupObject.cpp
@@ -378,6 +378,24 @@ bool CGroupObject::UpdateReference(bool bForceReload)
         }
 
         ClearInternal(ObjectsInGroupBk);
+
+        // append level prefox to spawn elements
+        if (Scene->LevelPrefix().size())
+        {
+            string256 prefix;
+            strconcat(sizeof(prefix), prefix, Scene->LevelPrefix().c_str(), "_");
+
+            ObjectsInGroup::iterator it, end;
+            it = m_ObjectsInGroup.begin();
+            end = m_ObjectsInGroup.end();
+            for (; it != end; it++)
+                if ((*it).pObject->FClassID == OBJCLASS_SPAWNPOINT && strncmp((*it).pObject->GetName(), prefix, strlen(prefix)) != 0)
+                {
+                    string256 new_name;
+                    strconcat(sizeof new_name, new_name, prefix, (*it).pObject->GetName());
+                    (*it).pObject->FName = new_name;
+                }
+        }
     }
     else
     {
