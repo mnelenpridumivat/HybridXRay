6357efe94388a2246165b6de649b722ad73c43a9
Romann <kriworuck.production@yandex.ru>
2023-11-08 20:43:20 +0300

    * [Compilers]  Обновление компиляторов от SkyLoader до версии 1.5.

---
+++ Compiler_Batch_NoHemi.cmd
@@ -0,0 +1,3 @@
+start /wait Bin\compilers_sky_x64\xrCompiler.exe -geometry -f map_test -nohemi -no_bcform -nostrip -noise -skipinvalid -skipthm -silent
+start /wait Bin\compilers_sky_x64\xrCompiler.exe -details -f map_test -nohemi -silent
+start /wait Bin\compilers_sky_x64\xrCompiler.exe -aispawn -f map_test -draft
